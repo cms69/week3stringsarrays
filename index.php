6a2e81ad98be5a69d056f752d80ef9ea66a91333
Chaitanya M Shah <cms69@afsconnect1.njit.edu>
2017-10-03 15:43:08 -0400

    stringsandarrays

---
+++ index.php
@@ -0,0 +1,224 @@
+<?php
+
+  $obj = new main();
+    $t_1 = "In1";
+      $t_2 = "In2";
+        $t_3 = "In3";
+	  $t_4 = "In4";
+	    $t_5 = "In5";
+	      $t_6 = "In6"
+	        $t_7 = "In7";
+		  $t_8 = "In8";
+		    $t_9 = "In9";
+		      $t_10 = "In10";
+		        $obj->p_1($t_1);
+			  $obj->p_2($t_2);
+			    $obj->p_3($t_3);
+			      $obj->p_4($t_4);
+			        $obj->p_5($t_5);
+				  $obj->p_6($t_6);
+				    $obj->p_7($t_7);
+				      $obj->p_8($t_8);
+				        $obj->p_9($t_9);
+					  $obj->p_10($t_10);
+
+					    class main {
+
+					        public function __construct() {
+
+
+						      echo '<h1>php</h1>';
+
+						          }
+							     
+							      
+							          
+								      public
+								      function
+								      print_1($t_1)
+								      {
+
+								            //
+									    do
+									    the
+									    function
+									    and
+									    print
+									    the
+									    output
+
+									        }
+										   
+
+										      
+										          public
+											  function
+											  print_2($t_2)
+											      {
+											            //
+												    do
+												    the
+												    function
+												    and
+												    print
+												    the
+												    output
+												          
+													      }
+
+
+													          public
+														  function
+														  print_3($t_3)
+														      {
+														          //
+															  do
+															  the
+															  function
+															  and
+															  print
+															  the
+															  output
+															      }
+
+															       
+
+															           public
+																   function
+																   print_4($t_4)
+																       {
+																               //
+																	       do
+																	       the
+																	       function
+																	       and
+																	       print
+																	       the
+																	       output
+																	           } 
+
+																		     
+
+																		         public
+																			 function
+																			 print_5($t_5)
+																			     {
+																			         	//
+																					do
+																					the
+																					function
+																					and
+																					print
+																					the
+																					output
+																					    }
+
+
+																					      
+																					          public
+																						  function
+																						  print_6($t_6)
+																						      {
+																						          	//
+																								do
+																								the
+																								function
+																								and
+																								print
+																								the
+																								output
+																								    }
+																								      
+
+																								        
+																									    public
+																									    function
+																									    print_7($t_7)
+																									        {
+
+																										    	//
+																											do
+																											the
+																											function
+																											and
+																											print
+																											the
+																											output
+
+																											    }
+
+
+																											      
+																											          public
+																												  function
+																												  print_8($t_8)
+																												      {
+																												          	//
+																														do
+																														the
+																														function
+																														and
+																														print
+																														the
+																														output
+
+																														    }
+
+																														      
+																														          public
+																															  function
+																															  print_9($t_9)
+																															      {
+																															              //
+																																      do
+																																      the
+																																      function
+																																      and
+																																      print
+																																      the
+																																      output
+
+																																          }   
+																																	    
+
+
+																																	        public
+																																		function
+																																		print_10($t_10)
+
+																																		    {
+
+																																		        	//
+																																				do
+																																				the
+																																				function
+																																				and
+																																				print
+																																				the
+																																				output 
+																																				    }
+
+																																				        public
+																																					function
+																																					__destruct()
+																																					{
+
+																																					     echo
+																																					     '</br>
+																																					     <h3>Finally
+																																					     I\'m
+																																					     Done
+																																					     with
+																																					     string
+																																					     functions.
+																																					     </h3>
+																																					     </br>';
+																																					         
+																																						   
+																																						      } 
+																																						      
+																																						    
+																																						      }
+																																						      ?>
+
+
